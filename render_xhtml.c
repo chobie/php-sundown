34c4da8acd3e1f71509646967a0586f39a68412f
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-12-08 16:04:32 +0900

    change email address

--- render_xhtml.c
+++ render_xhtml.c
@@ -12,7 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors: Shuhei Tanuma <chobieee@gmail.com>                          |
+   | Authors: Shuhei Tanuma <chobieeee@php.net>                           |
    +----------------------------------------------------------------------+
  */
 #include "php_sundown.h"
